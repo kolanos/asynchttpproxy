73a74a2fd7ff2a4ff6b6205457745d44d00d3818
Michael Lavers <kolanos@gmail.com>
2016-08-15 09:49:21 -0700

    Use new async syntax

--- asynchttpproxy/server.py
+++ asynchttpproxy/server.py
@@ -1,4 +1,3 @@
-import asyncio
 import aiohttp
 import aiohttp.server
 import io
@@ -14,8 +13,7 @@ uptime_start = time.time()
 
 
 class RequestHandler(aiohttp.server.ServerHttpProtocol):
-    @asyncio.coroutine
-    def handle_request(self, message, payload):
+    async def handle_request(self, message, payload):
         """Override the handle_request method to add proxy behavior."""
         global bytes_transferred
         url = message.path
@@ -24,24 +22,24 @@ class RequestHandler(aiohttp.server.ServerHttpProtocol):
         logging.info('{} {}'.format(message.method, url))
 
         if message.method == 'GET' and url == '/stats':
-            yield from self.send_stats_response(200, message.version)
+            await self.send_stats_response(200, message.version)
             return
 
         if parsed_url.scheme.lower() != 'http':
             logging.info('Refusing non-HTTP request: {}'.format(url))
-            yield from self.send_response(501, message.version)
+            await self.send_response(501, message.version)
             return
 
         if 'range' in message.headers and 'range' in query and \
                 message.headers['range'][6:] != query['range']:
-            yield from self.send_response(416, message.version)
+            await self.send_response(416, message.version)
             return
         if 'range' in query and 'range' not in message.headers:
             message.headers['range'] = 'bytes={}'.format(query['range'])
 
         logging.debug('Executing proxy request.')
-        response = yield from aiohttp.request(message.method, url,
-                                              headers=message.headers)
+        response = await aiohttp.request(message.method, url,
+                                         headers=message.headers)
         proxy_response = aiohttp.Response(self.writer, response.status,
                                           http_version=response.version)
 
@@ -54,15 +52,15 @@ class RequestHandler(aiohttp.server.ServerHttpProtocol):
         proxy_response.send_headers()
 
         while True:
-            chunk = yield from response.content.read(io.DEFAULT_BUFFER_SIZE)
+            chunk = await response.content.read(io.DEFAULT_BUFFER_SIZE)
             if not chunk:
                 break
             proxy_response.write(chunk)
             bytes_transferred += len(chunk)
-        yield from proxy_response.write_eof()
+        await proxy_response.write_eof()
 
-    @asyncio.coroutine
-    def send_response(self, status, http_version, headers=None, body=b''):
+    async def send_response(self, status, http_version, headers=None,
+                            body=b''):
         """Helper method to send an HTTP response."""
         response = aiohttp.Response(self.writer, status,
                                     http_version=http_version)
@@ -72,14 +70,13 @@ class RequestHandler(aiohttp.server.ServerHttpProtocol):
         response.add_header('Content-Length', str(len(body)))
         response.send_headers()
         response.write(body)
-        yield from response.write_eof()
+        await response.write_eof()
 
-    @asyncio.coroutine
-    def send_stats_response(self, status, http_version):
+    async def send_stats_response(self, status, http_version):
         """Sends bytes transfered and uptime stats."""
         body = json.dumps({
             'bytes_transferred': human_bytes(bytes_transferred),
             'uptime': uptime(uptime_start, time.time())
         }, indent=4).encode('ascii')
         headers = [('Content-Type', 'application/json')]
-        yield from self.send_response(status, http_version, headers, body)
+        await self.send_response(status, http_version, headers, body)
